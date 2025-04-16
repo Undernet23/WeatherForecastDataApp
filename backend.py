e95d569ee4292074aabff6fda27f8b703ae37006
Marian <dobrea.marian.23@gmail.com>
2025-04-16 21:58:26 +0300

    Version 1.3. Fixed some bugs - images, timestamps

--- backend.py
+++ backend.py
@@ -1,17 +1,11 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 import requests
 
-load_dotenv()  # Load from .env file
-
-
 def get_data(place, days):
-    API_KEY = os.getenv("API_KEY")
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    api_key = st.secrets["API_KEY"]
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
 
-    # Same as before
     filtered_data = data["list"]
-    nr_values = 8 * days
-    return filtered_data[:nr_values]
+    return filtered_data[:8 * days]
